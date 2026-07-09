51943de91eef98620cff523eae19578ba3bec9e3
mojyack <mojyack@gmail.com>
2026-07-09 11:34:41 +0900

    noxx: add span

---
+++ submodules/nolibc++/src/span.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "int.hpp"
+
+namespace noxx {
+template <class T>
+struct Span {
+    T*    data;
+    usize size;
+
+    constexpr auto subspan(const usize offset, const int count = -1) -> Span<T> {
+        return Span<T>{
+            .data = data + offset,
+            .size = count >= 0 ? count : size - offset,
+        };
+    }
+
+    constexpr auto operator[](const usize i) -> T& {
+        return data[i];
+    }
+
+    constexpr auto operator[](const usize i) const -> const T& {
+        return data[i];
+    }
+};
+} // namespace noxx
