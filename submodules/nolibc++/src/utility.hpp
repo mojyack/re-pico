9a834d78c5becd48140180cc555ab7296f06774f
mojyack <mojyack@gmail.com>
2026-07-09 11:33:46 +0900

    noxx: utility: make move() and exchange() constexpr

--- submodules/nolibc++/src/utility.hpp
+++ submodules/nolibc++/src/utility.hpp
@@ -2,12 +2,12 @@
 
 namespace noxx {
 template <class T>
-auto move(T& v) -> T&& {
+constexpr auto move(T& v) -> T&& {
     return (T&&)v;
 }
 
 template <class T, class U = T>
-auto exchange(T& a, U&& b) -> T {
+constexpr auto exchange(T& a, U&& b) -> T {
     auto old = move(a);
     a        = move(b);
     return old;
