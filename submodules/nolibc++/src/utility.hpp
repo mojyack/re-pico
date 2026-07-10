d5662cf08e833a362b1db6cdbde0241be8f3533d
mojyack <mojyack@gmail.com>
2026-07-10 10:47:07 +0900

    noxx: utility: add forward/destroy_at

--- submodules/nolibc++/src/utility.hpp
+++ submodules/nolibc++/src/utility.hpp
@@ -1,9 +1,25 @@
 #pragma once
+#include "type-traits.hpp"
 
 namespace noxx {
 template <class T>
 constexpr auto move(T& v) -> T&& {
     return (T&&)v;
+}
+
+template <class T>
+constexpr auto forward(RemoveReference<T>& v) -> T&& {
+    return (T&&)v;
+}
+
+template <class T>
+constexpr auto forward(RemoveReference<T>&& v) -> T&& {
+    return (T&&)v;
+}
+
+template <class T>
+constexpr auto destroy_at(T* const ptr) -> void {
+    ptr->~T();
 }
 
 template <class T, class U = T>
