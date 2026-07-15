5e75b7a83e5b70498348fac29f0702bec6d8aa38
mojyack <mojyack@gmail.com>
2026-07-15 17:41:49 +0900

    noxx: platform: add memset and memcmp

--- submodules/nolibc++/src/platform.hpp
+++ submodules/nolibc++/src/platform.hpp
@@ -4,4 +4,6 @@
 namespace noxx {
 auto console_out(const char* ptr) -> bool;
 auto memcpy(void* dest, const void* src, usize size) -> void;
+auto memset(void* a, u8 c, usize size) -> void;
+auto memcmp(const void* a, const void* b, usize size) -> int;
 } // namespace noxx
