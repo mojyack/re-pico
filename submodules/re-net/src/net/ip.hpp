d46305278bec0684a07a9a60ac4af40aff5fe4ea
mojyack <mojyack@gmail.com>
2026-07-16 18:13:43 +0900

    re-net: mac-addr/ip: implement formatter

--- submodules/re-net/src/net/ip.hpp
+++ submodules/re-net/src/net/ip.hpp
@@ -55,3 +55,26 @@ auto output(Stack& stack, IPv4Addr dst, u8 proto, AutoPacket packet) -> bool;
 } // namespace net::ipv4
 
 #include <noxx/bytes-alias.hpp>
+
+// formatting support
+#include <noxx/comptime-string.hpp>
+#include <noxx/format/integer.hpp>
+#include <noxx/string.hpp>
+
+#include <noxx/assert.hpp>
+
+namespace noxx::fmt {
+template <comptime::String params>
+auto format_segment(String& result, const net::IPv4AddrRef var) -> bool {
+    constexpr auto error_value = false;
+    static_assert(params.size() == 0, "format patameters not supported");
+    for(auto i = 0uz; i < net::IPv4Addr::size(); i += 1) {
+        ensure(format_segment<comptime::String("d")>(result, var[i]));
+        ensure(result.append('.'));
+    }
+    result.resize(result.size() - 1); // remove last '.'
+    return true;
+}
+} // namespace noxx::fmt
+
+#include <noxx/assert.hpp>
--- submodules/re-net/src/net/mac-addr.hpp
+++ submodules/re-net/src/net/mac-addr.hpp
@@ -9,3 +9,26 @@ bytes_alias(MacAddr, 6);
 } // namespace net
 
 #include <noxx/bytes-alias.hpp>
+
+// formatting support
+#include <noxx/comptime-string.hpp>
+#include <noxx/format/integer.hpp>
+#include <noxx/string.hpp>
+
+#include <noxx/assert.hpp>
+
+namespace noxx::fmt {
+template <comptime::String params>
+auto format_segment(String& result, const net::MacAddrRef var) -> bool {
+    constexpr auto error_value = false;
+    static_assert(params.size() == 0, "format patameters not supported");
+    for(auto i = 0uz; i < net::MacAddr::size(); i += 1) {
+        ensure(format_segment<comptime::String("02x")>(result, var[i]));
+        ensure(result.append(':'));
+    }
+    result.resize(result.size() - 1); // remove last ':'
+    return true;
+}
+} // namespace noxx::fmt
+
+#include <noxx/assert.hpp>
